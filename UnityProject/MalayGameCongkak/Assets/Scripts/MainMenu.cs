61cb6c7425d1ad215fda59081724d102d69e3562
Dan5033 <dhuaraland5033@gmail.com>
2018-11-06 17:49:48 +0800

    Main Menu Ad Banner

--- UnityProject/MalayGameCongkak/Assets/Scripts/MainMenu.cs
+++ UnityProject/MalayGameCongkak/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
 public class MainMenu : MonoBehaviour {
 
@@ -9,12 +10,24 @@ public class MainMenu : MonoBehaviour {
     [SerializeField] private string modeSelectName = "";
     [SerializeField] private string diffSelectName = "";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [Header("Adevertising")]
+    public string bannerPlacement = "mainMenuBanner";
+    public bool testMode = false;
+
+    #if UNITY_IOS
+        public const string gameID = "2894540";
+    #elif UNITY_ANDROID
+        public const string gameID = "2864542";
+    #elif UNITY_EDITOR
+        public const string gameID = "1111111";
+    #endif
+
+    void Start ()
+    {
+        Advertisement.Initialize(gameID, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -32,5 +45,15 @@ public class MainMenu : MonoBehaviour {
     public void CloseGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(bannerPlacement))
+        {
+            Debug.Log(Advertisement.IsReady(bannerPlacement));
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Show(bannerPlacement);
     }
 }
